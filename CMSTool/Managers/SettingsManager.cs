54484bd66188e868e3f8ce533dfab63940f17b1a
Viktor Fedorov <impqxr@gmail.com>
2025-01-29 23:12:07 +0100

    dont create config.json in Assets

--- CMSTool/Managers/SettingsManager.cs
+++ CMSTool/Managers/SettingsManager.cs
@@ -56,11 +56,7 @@ namespace FGCMSTool.Managers
 
         public void Load(string baseDir)
         {
-#if RELEASE_WIN_X64 || DEBUG
-            ConfigPath = Path.Combine(baseDir, "Assets", "config.json");
-#else
             ConfigPath = Path.Combine(baseDir, "config.json");
-#endif
 
             try
             {
